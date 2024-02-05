22bdd85a1e972edf163b4580ec72c81cd2316586
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-05 09:17:34 -0400

    Change: Documented in Spanish, CompraDto.java with @JsonView; Add: @Operation with responses & requestBody + Examples JSON over methods + Handled NotFoundException with Controller Advice + CompraViews.java

--- src/main/java/com/tiendadbii/tiendadbii/dto/CompraDto.java
+++ src/main/java/com/tiendadbii/tiendadbii/dto/CompraDto.java
@@ -1,5 +1,9 @@
 package com.tiendadbii.tiendadbii.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.tiendadbii.tiendadbii.util.views.CompraViews;
+import com.tiendadbii.tiendadbii.util.views.ProveedorViews;
+import com.tiendadbii.tiendadbii.util.views.Views;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +19,25 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompraDto implements Serializable {
+  @JsonView({CompraViews.ConProveedorId.class})
   ProveedorDto proveedor;
+
+  @JsonView({CompraViews.ConProveedorId.class, ProveedorViews.ConListaCompra.class})
   List<DetalleCompraDto> listaDetalleCompra;
 
+  @JsonView(Views.Private.class)
   LocalDateTime fechaRegistro;
+
+  @JsonView({CompraViews.ConProveedorId.class, ProveedorViews.ConListaCompra.class})
   Integer idCompra;
+
+  @JsonView({CompraViews.ConProveedorId.class, ProveedorViews.ConListaCompra.class})
   LocalDateTime fechaCompra;
+
+  @JsonView({CompraViews.ConProveedorId.class, ProveedorViews.ConListaCompra.class})
   Float totalCompra;
+
+  @JsonView({CompraViews.ConProveedorId.class, ProveedorViews.ConListaCompra.class})
   Float totalDescCompra;
+
 }
---
+++ src/main/java/com/tiendadbii/tiendadbii/util/views/CompraViews.java
@@ -0,0 +1,10 @@
+package com.tiendadbii.tiendadbii.util.views;
+
+public class CompraViews {
+
+  public interface ConProveedor extends PersonaViews.Ver {
+  }
+
+  public interface ConProveedorId {
+  }
+}
