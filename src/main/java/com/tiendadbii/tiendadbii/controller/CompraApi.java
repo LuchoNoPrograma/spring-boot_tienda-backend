5516ee9768fbd2951e55decde5138049aa8ac4d2
Luis Morales <105525189+LuchoNoPrograma@users.noreply.github.com>
2024-02-05 09:17:34 -0400

    Change: Documented in Spanish; Add: @Operation with responses & requestBody + Examples JSON over methods + Handled NotFoundException with Controller Advice

--- src/main/java/com/tiendadbii/tiendadbii/controller/CompraApi.java
+++ src/main/java/com/tiendadbii/tiendadbii/controller/CompraApi.java
@@ -1,5 +1,6 @@
 package com.tiendadbii.tiendadbii.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.tiendadbii.tiendadbii.dto.CompraDto;
 import com.tiendadbii.tiendadbii.dto.DetalleCompraDto;
 import com.tiendadbii.tiendadbii.dto.ProveedorDto;
@@ -10,17 +11,21 @@ import com.tiendadbii.tiendadbii.model.entity.Proveedor;
 import com.tiendadbii.tiendadbii.model.repository.DetalleCompraRepository;
 import com.tiendadbii.tiendadbii.model.service.interfaces.ICompraService;
 import com.tiendadbii.tiendadbii.model.service.interfaces.IProveedorService;
+import com.tiendadbii.tiendadbii.util.swagger_example.CompraExample;
+import com.tiendadbii.tiendadbii.util.views.CompraViews;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,30 +35,37 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/compra")
-@Tag(name = "Compra", description = "With this API, you can perform Create and Read Operation")
+@Tag(name = "2. Compra", description = "Con este EndPoint se puede realizar todas las operaciones de Compra necesarias para registrar e incrementar el stock de los productos")
 public class CompraApi {
   private final ICompraService compraService;
   private final ModelMapper modelMapper;
   private final IProveedorService proveedorService;
   private final DetalleCompraRepository detalleCompraRepository;
 
-  @Operation(summary = "Find Compra with given ID", description = "Given an idCompra, it will return Compra from DB",
+  @Operation(summary = "Buscar una Compra dado {idCompra}", description = "Retornar una compra desde la base de datos",
     responses = {
-      @ApiResponse(responseCode = "200", description = "Compra found successfully",
-        content = {@Content(schema = @Schema(implementation = CompraDto.class))}),
-      @ApiResponse(responseCode = "404", description = "Compra not found", content = @Content),
+      @ApiResponse(responseCode = "200", description = "Compra encontrada exitosamente",
+        content = {@Content(examples = @ExampleObject(value = CompraExample.COMPRA_CON_PROVEEDOR_ID))}),
+      @ApiResponse(responseCode = "404", description = "Compra no encontrada con el id: {idCompra}",
+        content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)),
     })
+  @JsonView(CompraViews.ConProveedorId.class)
   @GetMapping("/{idCompra}")
-  public ResponseEntity<CompraDto> findById(@PathVariable Integer idCompra) {
+  public ResponseEntity<?> findById(@PathVariable Integer idCompra) {
     Compra compra = compraService.findById(idCompra);
     if (compra == null) {
-      return ResponseEntity.notFound().build();
+      throw new EntityNotFoundException("Compra no encontrada con el id: " + idCompra);
     }
     return ResponseEntity.ok().body(this.toDto(compra));
   }
 
-  @Operation(summary = "List all registered Compra with Pageable", description = "The serach is performed in DB using 4 parameters")
+  @Operation(summary = "Listar todas las compras registradas con formato de página", description = "La búsqueda es procesada en la base de datos, basandose en 4 parametros",
+    responses = {
+      @ApiResponse(responseCode = "200", description = "Lista de compras registradas", content =
+      @Content(examples = @ExampleObject(value = CompraExample.COMPRA_CON_PROVEEDOR_ID))),
+    })
   @GetMapping("/pageable")
+  @JsonView(CompraViews.ConProveedorId.class)
   public ResponseEntity<List<CompraDto>> findAll(
     @RequestParam(required = false, defaultValue = "0") int page,
     @RequestParam(required = false, defaultValue = "10") int size,
@@ -66,19 +78,62 @@ public class CompraApi {
     return ResponseEntity.ok().body(compraService.findAll(pageable).stream().map(this::toDto).toList());
   }
 
-  @Operation(summary = "Create new Compra", description = "To create a new Compra, provide the necessary fields in the request body.",
+  @Operation(summary = "Crear nueva Compra",
+    description = "Las compras son el medio para poder registrar productos e incrementar stock de productos. Existiran 2 ejemplos de como registrar compras de las operaciones que se pueden realizar.",
+    requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+      description = "En ambos ejemplos se debe agregar el idProveedor en el Endpoint: ",
+      content = @Content(
+        examples = {
+          @ExampleObject(
+            summary = "Compra para registrar nuevo producto",
+            name = "Compra para registrar nuevo producto",
+            value = CompraExample.COMPRA_PARA_REGISTRAR_NUEVO_PRODUCTO,
+            description = """
+              Se requiere que el objeto tenga al menos un item en el campo "listaDetalleCompra".
+              Asegúrate de completar los campos establecidos en el ejemplo, incluido los datos del producto a registrar.
+              El stock inicial del producto se establecerá en el campo "cantidad" de dicho item.
+              \nEl costo total y descuento de costo total de la compra se calcula internamente.
+              """),
+          @ExampleObject(
+            summary = "Compra para agregar stock de producto registrado",
+            name = "Compra sin Producto",
+            value = CompraExample.COMPRA_PARA_INCREMENTAR_STOCK_DE_PRODUCTO_REGISTRADO,
+            description = """
+              Es necesario que el objeto contenga al menos un item en el campo "listaDetalleCompra".
+              Asegúrate de completar los campos establecidos en el ejemplo, donde en el campo "producto" solo se requiere llenar el "codigoProducto".
+              Para incrementar el stock del producto, debe completa el campo "cantidad" del item en el campo "listaDetalleCompra".
+              \nEl costo total y descuento de costo total de la compra se calcula internamente.
+              """
+          )
+        })
+    ),
     responses = {
-      @ApiResponse(responseCode = "201", description = "Compra created succesfully, returned Compra",
-        content = {@Content(schema = @Schema(implementation = CompraDto.class))}),
-      @ApiResponse(responseCode = "400", description = "Bad request, invalid field"),
-    })
+      @ApiResponse(
+        responseCode = "201",
+        description = "Compra creada exitosamente, se retorna el objeto creado con el header Location: /api/compra/{idCompra}",
+        content = @Content(examples = {
+          @ExampleObject(
+            summary = "Response para POST /api/compra",
+            name = "Ejemplo Response para POST /api/compra",
+            value = CompraExample.COMPRA_CREADA_EXITOSAMENTE
+          )
+        })
+      ),
+      @ApiResponse(responseCode = "400", description = "Petición invalida, asegúrate de completar todos los campos requeridos.", content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+      @ApiResponse(responseCode = "404", description = "Proveedor no encontrado con el id: {idProveedor}", content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+    }
+  )
+  @JsonView(CompraViews.ConProveedorId.class)
   @PostMapping("/proveedor/{idProveedor}")
   public ResponseEntity<?> createCompra(@RequestBody CompraDto dto, @PathVariable Integer idProveedor) {
+    if (proveedorService.findById(idProveedor) == null) {
+      throw new EntityNotFoundException("Proveedor no encontrado con el id: " + idProveedor);
+    }
     if (dto.getListaDetalleCompra() == null)
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Compra must have a list of DetalleCompra");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Compra debe tener items en el campo \"listaCompra\"");
     for (DetalleCompraDto detalleCompraDto : dto.getListaDetalleCompra()) {
       if (detalleCompraDto.getProducto() == null) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Each detalleCompra in compra must have a producto");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cada item en el campo \"listaCompra\" debe tener el campo \"producto\"");
       }
     }
 
@@ -89,14 +144,12 @@ public class CompraApi {
     compraPersisted.setListaDetalleCompra(detalleCompraRepository.findAllByCompra_IdCompra(compraPersisted.getIdCompra()));
 
     CompraDto compraDto = this.toDto(compraPersisted);
-    compraDto.setProveedor(null);
 
     return ResponseEntity
       .status(HttpStatus.CREATED)
       .header("Location", "/api/compra/" + compraDto.getIdCompra())
       .body(compraDto);
   }
-
 
   private Compra toEntity(CompraDto dto) {
     Compra compra = modelMapper.map(dto, Compra.class);
